719d3e202d73fcc9d110195f18ab83492232c4af
BIT <BIT@DESKTOP-DOGVD1T>
2017-08-04 14:21:19 +0900

    spring

---
+++ src/com/guestbook/controller/GuestbookController.java
@@ -0,0 +1,51 @@
+package com.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.guestbook.dao.GuestbookDao;
+import com.guestbook.vo.GuestbookVo;
+
+
+@Controller
+public class GuestbookController {
+
+	@Autowired
+	private GuestbookDao dao;
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		
+		
+		List<GuestbookVo> list=dao.getList();
+		/*System.out.println(list.toString());*/
+		model.addAttribute("list", list);
+		
+		/*return"WEB-INF/views/list.jsp";*/
+		return"list";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(@ModelAttribute GuestbookVo vo) {
+		dao.delete(vo);
+		return"redirect:/list";
+	}
+	
+	@RequestMapping("/add")
+	public String add(@ModelAttribute GuestbookVo vo){
+		System.out.println(vo.toString());
+		dao.insert(vo);
+		return"redirect:/list";
+	}
+	
+	@RequestMapping("/deleteform")
+	public String deleteform() {
+		System.out.println("이히");
+		return"deleteform";
+	}
+}
